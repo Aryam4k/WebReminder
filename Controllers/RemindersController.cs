212169b50528831fe966ba29bf3a64a3ebfd65a8
Aryam4k <aryam4k@gmail.com>
2018-03-18 23:01:26 +0530

    WebReminder

---
+++ Controllers/RemindersController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class RemindersController : Controller
+    {
+        private AppDBContext db = new AppDBContext();
+
+        // GET: Reminders
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Reminders.ToListAsync());
+        }
+
+        // GET: Reminders/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reminder reminder = await db.Reminders.FindAsync(id);
+            if (reminder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reminder);
+        }
+
+        // GET: Reminders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reminders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Message,Date")] Reminder reminder)
+        {
+            if (ModelState.IsValid)
+            {
+                reminder.CreatedDate = DateTime.Now;
+                db.Reminders.Add(reminder);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(reminder);
+        }
+
+        // GET: Reminders/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reminder reminder = await db.Reminders.FindAsync(id);
+            if (reminder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reminder);
+        }
+
+        // POST: Reminders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Message,Date")] Reminder reminder)
+        {
+            if (ModelState.IsValid)
+            {
+                reminder.CreatedDate = DateTime.Now;
+                db.Entry(reminder).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(reminder);
+        }
+
+        // GET: Reminders/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reminder reminder = await db.Reminders.FindAsync(id);
+            if (reminder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reminder);
+        }
+
+        // POST: Reminders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Reminder reminder = await db.Reminders.FindAsync(id);
+            db.Reminders.Remove(reminder);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Reminder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Reminder
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Reminder")]
+        public string Message { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
